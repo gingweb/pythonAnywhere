91683b4848a7e0767faeebbe075c5113fe03524f
gingweb <gingzab@gmail.com>
2023-07-21 22:19:41 +0700

    Work! folder routes

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def connect_to_mysql():
     return connection
 
 # Register Blueprint
-from user_routes import user_bp
+from routes.user_routes import user_bp
 app.register_blueprint(user_bp)
 
 if __name__ == '__main__':
